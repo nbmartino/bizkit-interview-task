26bb5cccf1d3aa4fa581aa72a70b21ce06e74698
Neil Martino <nbmartino@gmail.com>
2024-05-29 19:46:27 +0800

    before cleanup

---
+++ phasebook/pd_search.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+import json as json
+
+from .data.search_data import USERS
+
+def pd_search_users(args, data):
+
+    df = pd.DataFrame(data)
+
+    query_param = ""
+    
+    # compose our filter
+    for criteria in args:
+        if(len(query_param)):
+            query_param += " | "
+        if criteria == 'id':
+            query_param += '( id.str.match("%s") )' % (args[criteria])
+        if criteria == 'age':
+            age = args[criteria]
+            #query_param += '( age >= %d & age <= %d )' % (age - 1, age + 1)
+            query_param += '( %d <= age < %d)'  % (age - 1, age + 1)
+        if criteria == 'name':
+            query_param += '( name.str.contains("%s") )' % (args[criteria])
+        if criteria == 'occupation':
+            query_param += '( occupation.str.contains("%s") )' % (args[criteria])
+
+    if (len(query_param)):
+        sdf = df.loc[df.query(query_param, engine='python').index]
+        return sdf.to_dict(orient='records')
+    else:
+        return df.to_dict(orient='records')
+
+def pd_sorted_search_users(args, data):
+    
+    df = pd.DataFrame(data)
+    
+    result = []
+
+    # compose our result list
+    for criteria in args:
+        lst = []
+        args_criteria = args[criteria]
+        if criteria == 'id':
+            lst = df[df['id'].str.match(pat=args_criteria)].to_dict(orient='records')
+        if criteria == 'age':
+            age = int(args_criteria)
+            lst = df[df['age'].between(age-1, age+1)].to_dict(orient='records')
+        if criteria == 'name':
+            lst = df[df['name'].str.contains(pat=args_criteria)].to_dict(orient='records')
+        if criteria == 'occupation':
+            lst = df[df['occupation'].str.contains(pat=args_criteria)].to_dict(orient='records')
+        
+        #append partial list to final result list 
+        for res in lst:
+            if res not in result:
+                result.append(res)
+
+    if (len(args) != 0):
+        return result
+    else:
+        return df.to_dict(orient='records')
+
+
+#print(pd_search_users({"id":"2","name":"John"}, USERS))
+#print(pd_search_users_sort({"id":"5", "name": "Joe", "age": 30, "occupation":"Arc"}, USERS))
--- phasebook/search.py
+++ phasebook/search.py
@@ -2,14 +2,19 @@ from flask import Blueprint, request
 
 from .data.search_data import USERS
 
+from .pd_search import pd_search_users,pd_sorted_search_users
 
-bp = Blueprint("search", __name__, url_prefix="/search")
+
+bp = Blueprint("search", __name__)
 
 
-@bp.route("")
+@bp.route("/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
+@bp.route("/search_v2")
+def sorted_search():
+    return sorted_search_users(request.args.to_dict()), 200
 
 def search_users(args):
     """Search users database
@@ -27,4 +32,10 @@ def search_users(args):
 
     # Implement search here!
 
-    return args
+    
+    return pd_search_users(args,USERS)
+
+
+def sorted_search_users(args):
+    return pd_sorted_search_users(args, USERS)
+ 
